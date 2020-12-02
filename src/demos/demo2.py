c1e71702a712f7797525701d1741eb2adc0a0071
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-12-01 23:46:18 -0500

    Lecture notes

--- src/demos/demo2.py
+++ src/demos/demo2.py
@@ -23,13 +23,71 @@ Input:
 Output: False
 Explanation: The root node's value is 10 but its right child's value is 8.
 """
+import math # to get the lower and upper bounds math.inf
 # Definition for a binary tree node.
+
 class TreeNode:
     def __init__(self, value=0, left=None, right=None):
         self.value = value
         self.left = left
         self.right = right
 
-def is_valid_BST(self, root):
-    # Your code here
-    pass
+    def is_valid_BST(self, root):
+        # Your code here
+        # set up a tuple with a stack and the lowest bounds
+        stack, inorder = [], float(-math.inf)
+
+        # while either the stack or the root exists
+        while stack or root:
+            # while the root exists
+            while root:
+                # traverse the left of the tree
+                # append the root to the stack
+                stack.append(root)
+                # increment the root to the left
+                root = root.left # traversing the left branch like a linked list
+            # set the root to what is at the top of the stack
+            root = stack.pop()
+
+            # if next element in inorder traversal is smaller that the previous one it is not a BST Binary Search Tree
+            if root.value <= inorder:
+                # return false
+                return False
+
+            # set the in order to the roots value
+            inorder = root.value
+            # set the root to teh roots right node
+            root = root.right # traverse to the right
+        # return true
+        return True
+    
+    # recurvsive solution
+    """
+    def is_valid_BST(self):
+            # Your code here
+            valid = True
+            if self.left:
+                valid = valid and self.left.value < self.value
+                if not valid:
+                    return False
+                valid = valid and self.left.is_valid_BST()
+            if self.right:
+                valid = valid and self.right.value > self.value
+                if not valid:
+                    return False
+                valid = valid and self.right.is_valid_BST()
+            return valid
+    """
+
+b1 = TreeNode(5)
+b1.left = TreeNode(3)
+b1.right = TreeNode(7)
+
+b2 = TreeNode(10)
+b2.left = TreeNode(2)
+b2.right = TreeNode(8)
+b2.right.left = TreeNode(6)
+b2.right.right = TreeNode(12)
+
+print(b1.is_valid_BST(b1)) # True
+print(b2.is_valid_BST(b2)) # True
